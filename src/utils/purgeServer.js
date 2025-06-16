929442b822e095a386cb378cc0d2ecfe505e06e0
hhacki <futomhacki@gmail.com>
2025-06-16 18:38:22 +0200

    Handle DiscordAPIError[50074], closes #1

--- src/utils/purgeServer.js
+++ src/utils/purgeServer.js
@@ -4,35 +4,40 @@ module.exports = (client, interaction) => {
     const guild = interaction.guild;
     const bot = guild.members.cache.get(client.user.id);
 
-    guild.channels.cache.forEach((channel) => {
-        if (
-            !bot.permissionsIn(channel).has(PermissionsBitField.Flags.ViewChannel) ||
-            !bot.permissionsIn(channel).has(PermissionsBitField.Flags.ManageChannels)
-        ) {
-            return;
-        }
-        if (channel.type === 4) {
-            guild.channels.cache
-                .filter((ch) => ch.parentId === channel.id && ch.type !== 4)
-                .forEach((childChannel) => {
-                    if (
-                        !bot.permissionsIn(channel).has(PermissionsBitField.Flags.ViewChannel) ||
-                        !bot.permissionsIn(channel).has(PermissionsBitField.Flags.ManageChannels)
-                    )
-                        return;
-
-                    childChannel
-                        .delete()
-                        .then((deletedChildChannel) => console.log(`Deleted channel: ${deletedChildChannel.name}`));
-                });
-            channel.delete().then((deletedCategory) => console.log(`Deleted category: ${deletedCategory.name}`));
-        } else if (!channel.parentId) {
+    guild.channels.cache.forEach(async (channel) => {
+        try {
             if (
                 !bot.permissionsIn(channel).has(PermissionsBitField.Flags.ViewChannel) ||
                 !bot.permissionsIn(channel).has(PermissionsBitField.Flags.ManageChannels)
-            )
+            ) {
                 return;
-            channel.delete().then((deletedChannel) => console.log(`Deleted channel: ${deletedChannel.name}`));
+            }
+            if (channel.type === 4) {
+                await guild.channels.cache
+                    .filter((ch) => ch.parentId === channel.id && ch.type !== 4)
+                    .forEach((childChannel) => {
+                        if (
+                            !bot.permissionsIn(channel).has(PermissionsBitField.Flags.ViewChannel) ||
+                            !bot.permissionsIn(channel).has(PermissionsBitField.Flags.ManageChannels)
+                        )
+                            return;
+
+                        childChannel
+                            .delete()
+                            .then((deletedChildChannel) => console.log(`Deleted channel: ${deletedChildChannel.name}`));
+                    });
+                await channel.delete().then((deletedCategory) => console.log(`Deleted category: ${deletedCategory.name}`));
+            } else if (!channel.parentId) {
+                if (
+                    !bot.permissionsIn(channel).has(PermissionsBitField.Flags.ViewChannel) ||
+                    !bot.permissionsIn(channel).has(PermissionsBitField.Flags.ManageChannels)
+                )
+                    return;
+                await channel.delete().then((deletedChannel) => console.log(`Deleted channel: ${deletedChannel.name}`));
+            }
+        } catch (e) {
+            if (e.code === 50074) console.log('Cannot delete a channel required for community servers, skipping.')
+            else throw e
         }
     });
 };
